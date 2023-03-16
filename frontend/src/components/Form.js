02a773302b2440625e5e6391b89f112731fee156
nncrvo <nunocrmartins@gmail.com>
2023-03-16 01:57:23 +0100

    second date picker now only shows on return trip

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,9 +1,18 @@
 import './Form.css';
+import { useState, useRef } from "react";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const Form = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    const firstPickerRef = useRef(null);
+    const secondPickerRef = useRef(null);
+    const firstFormRef = useRef(null);
+
     const handleOutsideClick = e => {
-        if (e.target.className === 'from-to') {
-            e.target.childNodes.forEach(node => {
+        if (e.target.classList.contains('clickable')) {
+            firstFormRef.current.childNodes.forEach(node => {
                 if (node.firstChild.className === 'input-label animate') {
                     node.firstChild.className = 'input-label deanimate';
                     node.lastChild.className = 'form-input hide';
@@ -20,10 +29,19 @@ const Form = () => {
         e.currentTarget.lastChild.focus();
     }
 
+    const tripTypeHandler = e => {
+        if (firstPickerRef.current.classList.contains('longPicker')) {
+            firstPickerRef.current.classList.remove('longPicker');
+            return secondPickerRef.current.classList.remove('displayNone');
+        }
+        firstPickerRef.current.classList.add('longPicker');
+        return secondPickerRef.current.classList.add('displayNone');
+    }
+
     return (
         <section className='form-container'>
-            <div className='form'>
-                <form className='from-to' onClick={handleOutsideClick}>
+            <div className='form' onClick={handleOutsideClick}>
+                <form className='from-to clickable' ref={firstFormRef}>
                     <div className='input-container from' onClick={handleInputClick}>
                         <p className='input-label default'>From*</p>
                         <input className='form-input hide' type="text"></input>
@@ -33,23 +51,29 @@ const Form = () => {
                         <input className='form-input hide' type="text"></input>
                     </div>
                 </form>
-                <form className='second-form'>
-                    <div className='trip-type'>
-                        <input type='radio' id='oneway' name="trip" value='One way' defaultChecked></input>
+                <form className='second-form clickable'>
+                    <div className='trip-type clickable' onChange={tripTypeHandler}>
+                        <input className='oneway' type='radio' id='oneway' name="trip" value='One way' defaultChecked></input>
                         <label htmlFor='oneway'>One way</label>
-                        <input type='radio' id='roundtrip' name="trip" value='Round trip'></input>
+                        <input className='roundtrip' type='radio' id='roundtrip' name="trip" value='Round trip'></input>
                         <label htmlFor='roundtrip'>Round trip</label>
                     </div>
-                    <div className='date-form'>
-                        <div className='input-container from'>
-                            <p className='date-label'>Departure date*</p>
-                            <input className='form-input show' type="text"></input>
-                        </div>
-                        <div className='input-container to'>
-                            <p className='date-label'>Return date*</p>
-                            <input className='form-input show' type="text"></input>
-                        </div>
+                    <div className='date-form clickable'>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <div className='clickable'>
+                                <p className='date-label'>Departure date*</p>
+                                <DatePicker className='date-picker longPicker' ref={firstPickerRef}/>
+                            </div>
+                            <div className='displayNone clickable' ref={secondPickerRef}>
+                                <p className='date-label'>Return date*</p>
+                                <DatePicker className='date-picker'/>
+                            </div>
+                        </LocalizationProvider>
                     </div>
+                    {/* <div className='quantity clickable'>
+                        <p className='input-label default'>To*</p>
+                        <input className='form-input hide' type="text"></input>
+                    </div> */}
                 </form>
             </div>
         </section>
